ea27072e672da49dbf3cb251c397994d3a9d1ba2
Jax Braithwaite <jbraith29@gmail.com>
2023-12-31 13:32:49 +0000

    conversion template

---
+++ pdf_to_excel.py
@@ -0,0 +1,81 @@
+# Import Module  
+import tabula 
+
+file_path = 'alphadoku-symmetric-expert-puzzle-20'
+  
+# Read PDF File 
+df = tabula.read_pdf(f'{file_path}.pdf')[0]
+  
+# Convert into Excel File 
+df.to_excel(f'{file_path}.xlsx',index=False)
+
+
+from openpyxl import load_workbook
+from openpyxl.styles import Font
+
+code = {'A': 1,
+        'B': 2,
+        'C': 3,
+        'D': 4,
+        'E': 5,
+        'F': 6,
+        'G': 7,
+        'H': 8,
+        'I': 9,
+        'J': 10,
+        'K': 11,
+        'L': 12,
+        'M': 13,
+        'N': 14,
+        'O': 15,
+        'P': 16,
+        'Q': 17,
+        'R': 18,
+        'S': 19,
+        'T': 20,
+        'U': 21,
+        'V': 22,
+        'W': 23,
+        'X': 24,
+        'Y': 25}
+
+
+# reformat
+def copy_formatting(source_wb, target_wb):
+        target_ws = target_wb['Sheet1']
+        source_ws = source_wb['Sheet1']
+        for row in source_ws.iter_rows():
+            for cell in row:
+                target_ws[cell.coordinate].font = cell.font.copy()
+                target_ws[cell.coordinate].border = cell.border.copy()
+                target_ws[cell.coordinate].fill = cell.fill.copy()
+                target_ws[cell.coordinate].number_format = cell.number_format
+                target_ws[cell.coordinate].alignment = cell.alignment.copy()
+
+alpha = load_workbook(f'{file_path}.xlsx')
+copy_formatting(load_workbook('formatting_template.xlsx'),alpha)
+
+
+for row in alpha['Sheet1'].iter_rows():
+    for cell in row:
+        if cell.value == None:
+            continue
+        elif cell.value.startswith('Unnamed'):
+            cell.value = None
+            continue
+        else:
+            for letter, number in code.items():
+                cell.value = cell.value.replace(letter,str(number))
+                cell.font = Font(name='Calibri (Body)',
+                                 size=14,
+                                 color = '000000')
+
+
+
+alpha.save('new_sudoku.xlsx')
+
+print("Success!")
+
+# # except:
+# #     print("Some kinda problem happened")
+
